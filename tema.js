f0880d185473ab7c7d0488dc7ab6dfbb53b4ca05
DanielaPruna <pruna_daniela@yahoo.com>
2023-07-25 07:28:37 -0700

    commit functii

---
+++ tema.js
@@ -0,0 +1,59 @@
+// //EX. 1
+
+// function findSquare (num) {
+//   return Math.pow(6, 2);
+// }
+
+// let squareOfNumber = findSquare();
+// console.log("The square is " , squareOfNumber)
+
+
+//EX. 2
+
+// function getRandom (start , end) {
+//   return Math.floor(Math.random() *(max - 1 - min + 1) + min)
+// }
+
+// let min = -3;
+// let max = 5;
+// let result = getRandom();
+
+// console.log("The random number is " , result);
+
+
+//EX. 3
+// function letterCount (text, char) {
+//   let counter = 0;
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[i].toLowerCase() === char) {
+//       counter ++;
+//     }
+//   }
+//   return counter;
+// }
+
+//   let text = "Un text pentru Java";
+//   let char = "j";
+
+//   let result = letterCount(text, char);
+//   console.log("The frequency is: " + result);
+
+
+//EX. 4 
+
+// function addNumber(...argument) {
+//   let sum = 0;
+
+//   for (let arg of argument) sum += arg; {
+  
+//     return sum;
+//   }
+// }
+
+// console.log("The sum is: " , addNumber(1, 3, 5, 7, 10, 30));
+
+
+  
+
+
+
